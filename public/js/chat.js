8e5ac8230b7b313a909993fd5222bf08602d4ffe
CHKMTY <matheus.wesleys@hotmail.com>
2021-04-23 23:18:53 -0300

    update files'

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,6 @@
+let socket = null;
+
 document.querySelector("#start_chat").addEventListener("click", (event) => {
-  
   const socket = io();
 
   const chat_help = document.getElementById("chat_help");
@@ -37,8 +38,19 @@ document.querySelector("#start_chat").addEventListener("click", (event) => {
         });
         
         document.getElementById("messages").innerHTML += rendered;
+        
+      } else {
+        const rendered = Mustache.render(template_admin, {
+          message_admin: message.text,
+        });
+
+        document.getElementById("messages").innerHTML += rendered;
+
       }
     });
+  });
 
+  socket.on("admin_send_to_client", (message) => {
+    console.log(message);
   });
 });
--- src/http.ts
+++ src/http.ts
@@ -14,6 +14,7 @@ app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 
 app.get("/pages/client", (request, response) => { return response.render("html/client.html") });
+app.get("/pages/admin", (request, response) => { return response.render("html/admin.html") });
 
 const http = createServer(app);
 const io = new Server(http);
